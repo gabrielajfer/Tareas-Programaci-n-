4b4ec3f6e90d06cc0c4002762c17f780a1867589
Gabriela Jiménez <131317179+gabrielajfer@users.noreply.github.com>
2023-05-06 13:26:56 -0400

    Add files via upload

---
+++ Ejercicio 1.py
@@ -0,0 +1,8 @@
+>>> num1 = int(input("Ingrese el primer numero entero: "))
+>>> num2 = int(input("Ingrese el segundo numero entero: "))
+>>> num3 = int(input("Ingrese el tercer numero entero: "))
+>>> list = [num2, num3, num1]
+>>> list_ordenada = sorted(list)
+>>> print("La lista ordenada es:", list_ordenada)
+>>> list_reordenada = sorted(list, reverse=True)
+>>> print(list)
---
+++ Ejercicio 2.py
@@ -0,0 +1,3 @@
+>>> mensaje1= input("Ingrese su frase favorita como cadena de caracteres en mayúsculas: ")
+>>> mensaje2 = mensaje1.lower()
+>>> print(mensaje2)
