ee628d5d6a41c838dae7076454365d80214d7dab
Gabriela Jiménez <131317179+gabrielajfer@users.noreply.github.com>
2023-05-08 23:03:40 -0400

    Add files via upload

---
+++ TAREA4.py
@@ -0,0 +1,10 @@
+while True:
+    x1 = (input("Ingrese un numero: "))
+    x2 = (input("Ingrese otro numero: "))
+    def suma(x1,x2):
+        if x1.isdigit() and x2.isdigit():
+         return int(x1)+int(x2)
+        else:
+         return "Error ingrese valores numericos"
+suma (x1, x2)
+
